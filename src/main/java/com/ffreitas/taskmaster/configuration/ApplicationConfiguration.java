5048c333fcbe00b866a086db1102c95128d0102e
FFreitas <francisco.freitas.ff@gmail.com>
2024-07-12 15:49:37 +0100

    docker compose && structure defined && init configuration

---
+++ src/main/java/com/ffreitas/taskmaster/configuration/ApplicationConfiguration.java
@@ -0,0 +1,4 @@
+package com.ffreitas.taskmaster.configuration;
+
+public class ApplicationConfiguration {
+}
---
+++ src/main/java/com/ffreitas/taskmaster/configuration/OpenApiConfiguration.java
@@ -0,0 +1,4 @@
+package com.ffreitas.taskmaster.configuration;
+
+public class OpenApiConfiguration {
+}
